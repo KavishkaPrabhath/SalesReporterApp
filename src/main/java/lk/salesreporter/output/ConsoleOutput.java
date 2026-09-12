be869b9b8bc3a8abb29b693b01912dfbc73b3da8
Akindu Edirisinghe <akinduedirisingha89@gmail.com>
2026-09-12 11:20:07 +0530

    Implemnt console output strategy

---
+++ src/main/java/lk/salesreporter/output/ConsoleOutput.java
@@ -0,0 +1,8 @@
+package lk.salesreporter.output;
+
+public class ConsoleOutput implements OutputStrategy {
+    @Override
+    public void output(String report) {
+        System.out.println(report);
+    }
+}
