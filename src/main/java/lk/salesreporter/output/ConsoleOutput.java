600a6967ac2c8c53e3bfc7e2dfa6ec6e4d010015
thisura25 <hgthisura@gmail.com>
2026-09-12 10:44:22 +0530

    Implement console output strategy

--- src/main/java/lk/salesreporter/output/ConsoleOutput.java
+++ src/main/java/lk/salesreporter/output/ConsoleOutput.java
@@ -2,7 +2,14 @@ package lk.salesreporter.output;
 
 public class ConsoleOutput implements OutputStrategy {
     @Override
+
+
     public void output(String report) {
+
+
         System.out.println(report);
+
+
+
     }
 }
